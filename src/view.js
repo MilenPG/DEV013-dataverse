fb8e7b2b8cb2a09f5fa7a7f38982431f4b8662aa
Milen <millalen.pg@gmail.com>
2024-01-26 03:49:05 -0300

    HTML 11/13 tests: <li> pendientes. Data ya se ve en consola

--- src/view.js
+++ src/view.js
@@ -1,5 +1,7 @@
+import data from './data/dataset.js';
 export const renderItems = (data) => {
   console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
   return 'example';
 };
+console.log(data); //con esto ya visualizamos la data en console.log

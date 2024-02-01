303a5992ab90be9df01a38cdf0be30b5a9e280a8
Milen <millalen.pg@gmail.com>
2024-02-01 20:12:10 -0300

    Primeros pasos filterData, modificaciones en main y dataFunction (trabajo en OH con Chris)

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,4 +1,4 @@
-// Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
+/*Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
 export const example = () => {
   return 'example';
@@ -6,13 +6,26 @@ export const example = () => {
 
 export const anotherExample = () => {
   return [];
-};
+}; */
 
+export const filterData = (data, filterBy, value) => {
+  console.log(data)
+  console.log(filterBy)
+  console.log(value)
+
+}
+
+export const orderData = (data, sortBy, sortOrder) => {
+  console.log(data)
+  console.log(sortBy)
+  console.log(sortOrder)
+
+}
 
 /*
 - Crear constante de función "sortBy" que tiene 3 parámetros (data, sortBy, sortOrder) 
 - Declarar sortBy qué significa orden ascendente y/o descendente con operadores lógicos.
-- Recorrer arreglo con forEach
+- Recorrer arreglo con forEach?
 - Por cada objeto, coger el Id.
 - Comparar por lo que definimos como sortBy, según la primera letra (índice 0) del nombre.
 - Pushear objetos a un nuevo array que:
@@ -20,5 +33,3 @@ export const anotherExample = () => {
   - Ordene descendentemente según lo anterior
 - Devuelve función con el arreglo ordenado.
 */
-
-
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-//import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 import data from './data/dataset.js';
+import { filterData, orderData } from './dataFunctions.js';
 
 const root = document.querySelector('#root');
 root.appendChild(renderItems(data));
 
-//console.log(example, renderItems(data), data);
+//llamado función filter:
+const filter=document.querySelector('#filter')
+filter.addEventListener('change',()=> { //dentro del paréntesis podemos recibir el objeto "event" o "e"
+  //console.log(filter.value) // ...con "e" + ".target" nos retornaría exactamente lo mismo. (BUSCAR "TARGET") 
+  filterData(data, 'conservationStatus', filter.value)
+  console.log(filterData)
+})
+
+//llamado función order
+const order=document.getElementById('order')
+order.addEventListener('change', (e)=> {
+  orderData(data, 'id', e.target.value)  
+  console.log(orderData)
+})

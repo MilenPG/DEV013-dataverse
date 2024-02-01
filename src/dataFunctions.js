16b09d19ddfdbb0602f847b9762323d8d67cedd0
Milen <millalen.pg@gmail.com>
2024-01-31 22:01:31 -0300

    CSS básico test passed. Corregir error imageUrl

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -7,3 +7,18 @@ export const example = () => {
 export const anotherExample = () => {
   return [];
 };
+
+
+/*
+- Crear constante de función "sortBy" que tiene 3 parámetros (data, sortBy, sortOrder) 
+- Declarar sortBy qué significa orden ascendente y/o descendente con operadores lógicos.
+- Recorrer arreglo con forEach
+- Por cada objeto, coger el Id.
+- Comparar por lo que definimos como sortBy, según la primera letra (índice 0) del nombre.
+- Pushear objetos a un nuevo array que:
+  - Ordene ascendentemente según el primer índice del nombre
+  - Ordene descendentemente según lo anterior
+- Devuelve función con el arreglo ordenado.
+*/
+
+

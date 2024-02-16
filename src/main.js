c62b25aed71ddc10faee8f12a87bc7b30aad90a3
DaisyCorrea <daisy_jch@hotmail.com>
2024-02-15 20:57:50 -0500

    diseño de estadísticas, inmobilizar títulos, arreglo de fuente y jv creación del cuadro de estadísticas

--- src/main.js
+++ src/main.js
@@ -40,3 +40,36 @@ search.addEventListener('keyup', e => {
   }
   //console.log(e.target.value);
 })*/
+
+const openStatistics = document.querySelector('#buttonStatistics');
+const windowStatistics = document.querySelector('#statisticsWindowOpen');
+const cerrarStatistics = document.querySelector('#statisticsWindowClose');
+openStatistics.addEventListener("click",()=>{
+  windowStatistics.showModal();
+})
+/*const std = document.getElementById('myChart')
+const names = ['Ronn', 'Daisy', 'Madelein']
+const ages = [17, 42, 13]
+const myChart = new CharacterData(std, {
+  type: 'pie',
+  data: {
+    labels: names,
+    datasets: [{
+      label: 'Edad',
+      data: ages,
+      backgroundColor: [
+        'rgba (255, 99, 132, 0.2)',
+        'rgba (54, 162, 235, 0.2)',
+        'rgba (255, 206, 86, 0.2)',
+        'rgba (75, 192, 192, 0.2)',
+        'rgba (153, 102, 255, 0.2)',
+        'rgba (255, 159, 64, 0.2)'
+      ],
+      borderWidth: 1.5
+    }]
+  }
+})*/
+
+cerrarStatistics.addEventListener("click",()=>{
+  windowStatistics.close();
+})
